f5f9ec980d7330b7494665c3c5ac55954aa74f25
alogic0 <alogic0@gmail.com>
2014-02-15 03:37:28 +0200

    Update files2dirs.sh
    Used 'pushd/popd' instead of 'cd' in recursive calls.

--- files2dirs.sh
+++ files2dirs.sh
@@ -13,14 +13,15 @@ ext_dir () {
 }
 
 f_tree () {
-  sed -r 's@(<h4>)@\n\1@; s@(</h4>)@\1\n' "$1" | grep '<h4' | while read s; do
+  sed -r 's@(<h4>)@\n\1@; s@(</h4>)@\1\n@' "$1" | grep '<h4' | while read s; do
       fl=$(ext_file "$s")
       if [ -f $source/$fl ]; then
         dir_n="$(ext_dir "$s")"
         mkdir "$dir_n"
         cp $source/$fl "$dir_n"
-        cd "$dir_n"
+        pushd "$dir_n"
         f_tree $fl
+        popd
       fi
   done
 }
